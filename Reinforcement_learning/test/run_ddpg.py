0bd67441a2df8ca20aad121b263650b059ad8781
zhangkai <zhang_kai_jisuan@163.com>
2019-04-04 16:54:15 +0800

    first stage finish

---
+++ Reinforcement_learning/test/run_ddpg.py
@@ -0,0 +1,51 @@
+import gym
+from Reinforcement_learning import DDPG
+import numpy as np
+
+MAX_EPISODES = 200
+MAX_EP_STEPS = 200
+LR_A = 0.001    # learning rate for actor
+LR_C = 0.002    # learning rate for critic
+GAMMA = 0.9     # reward discount
+TAU = 0.01      # soft replacement
+MEMORY_CAPACITY = 10000
+BATCH_SIZE = 32
+
+RENDER = False
+ENV_NAME = 'Pendulum-v0'
+
+
+env = gym.make(ENV_NAME)
+env = env.unwrapped
+env.seed(1)
+
+s_dim = env.observation_space.shape[0]
+a_dim = env.action_space.shape[0]
+a_bound = env.action_space.high
+
+ddpg = DDPG(a_dim, s_dim, a_bound)
+
+var = 3  # control exploration
+for i in range(MAX_EPISODES):
+    s = env.reset()
+    ep_reward = 0
+    for j in range(MAX_EP_STEPS):
+        if RENDER:
+            env.render()
+
+        # Add exploration noise
+        a = ddpg.choose_action(s)
+        a = np.clip(np.random.normal(a, var), -2, 2)    # add randomness to action selection for exploration
+        s_, r, done, info = env.step(a)
+
+        ddpg.store_transition(s, a, r / 10, s_)
+
+        if ddpg.pointer > MEMORY_CAPACITY:
+            var *= .9995    # decay the action randomness
+            ddpg.learn()
+
+        s = s_
+        ep_reward += r
+        if j == MAX_EP_STEPS-1:
+            print('Episode:', i, ' Reward: %i' % int(ep_reward), 'Explore: %.2f' % var, )
+            if ep_reward > -300:RENDER = True
